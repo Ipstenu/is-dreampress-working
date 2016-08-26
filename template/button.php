d54ec171c819e45c5359e9ab29a072280400f480
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2016-08-26 10:18:01 -0700

    Turn on captcha

--- template/button.php
+++ template/button.php
@@ -1,8 +1,8 @@
 <center>
 <form method="POST" action="<?php echo $filename; ?>" id="check_dreampress_form">
-      <input name="url" id="url" value="<?php if (isset($varnish_host) ) echo $varnish_host; ?>" type="text" size="100" placeholder="http://dreamhost.com">
-     <!-- <div class="g-recaptcha" data-sitekey="6LfsogkTAAAAAMuZHeO_l9qN3k-V-xhyZkEtM_IE"></div> -->
-      <p>&nbsp;</p>
-      <input name="check_it" id="check_it" value="Check It!" type="submit" class="btn dreampress-tech-features-trigger">
+	<input name="url" id="url" value="<?php if (isset($varnish_host) ) echo $varnish_host; ?>" type="text" size="100" placeholder="http://dreamhost.com">
+	<div class="g-recaptcha" data-sitekey="6LfsogkTAAAAAMuZHeO_l9qN3k-V-xhyZkEtM_IE"></div> -->
+	<p>&nbsp;</p>
+	<input name="check_it" id="check_it" value="Check It!" type="submit" class="btn dreampress-tech-features-trigger">
 </form>
 </center>
