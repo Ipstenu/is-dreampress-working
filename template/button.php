c84fece0dfd0316ab1d883bd723cdf5f0f9fbf2d
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2016-08-31 12:52:18 -0700

    FUCK - don’t black that out!

--- template/button.php
+++ template/button.php
@@ -1,7 +1,7 @@
 <center>
 <form method="POST" action="<?php echo $filename; ?>" id="check_dreampress_form">
 	<input name="url" id="url" value="<?php if (isset($varnish_host) ) echo $varnish_host; ?>" type="text" size="100" placeholder="http://dreamhost.com">
-	<!-- <p><div class="g-recaptcha" data-sitekey="6LfsogkTAAAAAMuZHeO_l9qN3k-V-xhyZkEtM_IE"></div></p> -->
+	<p><div class="g-recaptcha" data-sitekey="6LfsogkTAAAAAMuZHeO_l9qN3k-V-xhyZkEtM_IE"></div></p>
 	<input name="check_it" id="check_it" value="Check It!" type="submit" class="btn dreampress-tech-features-trigger">
 </form>
 </center>
